79ca89395739eba6c27688e7275da1dfde3384e0
Julian Gomez Sibecas <juliangsibecas@gmail.com>
2021-12-21 19:23:41 -0300

    Update version

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 export { Core as Solidmation } from './core'
 
-export { Home } from './home'
+export * from './home'
 export * from './home/types'
 
-export { AirConditioner } from './airconditioner'
+export * from './airconditioner'
 export * from './airconditioner/types'
-
